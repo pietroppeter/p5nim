1761450e716d05061308a680341a249ad90f8f70
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2022-11-28 21:21:53 +0100

    add useP5 for nimib

--- src/p5.nim
+++ src/p5.nim
@@ -1,6 +1,10 @@
-import p5 / [p5js, p5sugar]
-export p5js, p5sugar
+when defined(js):
+    import p5 / [p5js, p5sugar]
+    export p5js, p5sugar
 
-import std / lenientops
-export lenientops
-converter toFloat*(n: int): float = float(n)
+    import std / lenientops
+    export lenientops
+    converter toFloat*(n: int): float = float(n)
+else:
+    import p5 / p5nimib
+    export p5nimib
---
+++ src/p5/p5nimib.nim
@@ -0,0 +1,2 @@
+template nbUseP5* =
+  nbRawHtml: """<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.5.0/p5.min.js"></script>"""
