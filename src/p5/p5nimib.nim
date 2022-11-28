d96976fd2cbf7424c89f9c6846eabb573ac84976
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2022-11-29 00:10:57 +0100

    useP5 adds script in head

--- src/p5/p5nimib.nim
+++ src/p5/p5nimib.nim
@@ -1,2 +1,3 @@
 template nbUseP5* =
-  nbRawHtml: """<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.5.0/p5.min.js"></script>"""
+  nb.partials["head"] &= """<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.5.0/p5.min.js"></script>"""
+
