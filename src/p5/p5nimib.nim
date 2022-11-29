32c30076155c5fb12c7a6d18368b1dd419b0034e
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2022-11-29 01:20:20 +0100

    useP5 already imports p5 + nbP5instance

--- src/p5/p5nimib.nim
+++ src/p5/p5nimib.nim
@@ -1,3 +1,11 @@
 template nbUseP5* =
   nb.partials["head"] &= """<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.5.0/p5.min.js"></script>"""
+  nbJsFromCodeGlobal:
+    import p5
 
+template nbP5Instance*(body: untyped) =
+  let p5instanceId = "p5instance-" & $nb.newId()
+  nbRawHtml: "<div id=\"" & p5instanceId & "\"></div>"
+  nbJsFromCode(p5instanceId):
+    instance(p5instanceId):
+      body
